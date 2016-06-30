7804e6d98722498c90954bdb0497e4b4818e615e
Peter Maguire <peter@unacceptableuse.com>
2016-06-30 22:04:45 +0100

    !test registered buttons in onReady instead of when command is executed

--- commands/test.js
+++ commands/test.js
@@ -2,22 +2,18 @@ exports.command = {
 	name: "test",
 	desc: "A test command",
 	usage: "test",
-	func: function(user, userID, channel, args, message, bot){
-
-        bot.sendMessage({
-        	to: channel,
-        	message: "What in gods name is going on here"
+    onReady: function(bot){
+        bot.registerInteractiveMessage("test", function(name, val, info){
+            bot.sendMessage({
+                to: channel,
+                message: `:whey: Receieved callback response: \`${name}\`=\`${val}\``
+            });
+            return "";
         });
 
-		bot.registerInteractiveMessage("test", function(name, val){
-            bot.sendMessage({
-            	to: channel,
-            	message: `:whey: Receieved callback response: \`${name}\`=\`${val}\``
-            });
-			return "";
-		});
-
-        bot.sendButtons(channel, "Test buttons", "a", "test", "#fefefe    ", [{"name": "a", "text": "Test", "value": "dick"}]);
+    },
+	func: function(user, userID, channel, args, message, bot){
+        bot.sendButtons(channel, "Test buttons", "test buttons", "test", "#fefefe", [{"name": "a", "text": "Test", "value": "dick"}]);
 		return true;
 	}
 };
