198784a7885b22a3418017c909cd4c891e6e7610
Peter Maguire <peter@unacceptableuse.com>
2016-07-02 02:09:20 +0100

    Added painstakingly accurate clocks to `!time`

--- commands/time.js
+++ commands/time.js
@@ -1,15 +1,22 @@
 /**
  * Created by Peter on 02/07/2016.
  */
+
 exports.command = {
     name: "time",
     desc: "The time ime",
     usage: "time",
     func: function(user, userID, channel, args, message, bot){
         var now = new Date();
+        var emoji = ":clock"+now.getHours();
+        if(now.getMinutes() >= 30)
+            emoji+="30";
+        emoji += ":";
+
+
         bot.sendMessage({
         	to: channel,
-        	message: `The time is *${(now.getHours()<10?'0':'')+now.getHours()}:${(now.getMinutes()<10?'0':'') +now.getMinutes()}:${(now.getSeconds()<10?'0':'')+now.getSeconds()}*`
+        	message: `${emoji} The time is *${(now.getHours()<10?'0':'')+now.getHours()}:${(now.getMinutes()<10?'0':'') +now.getMinutes()}:${(now.getSeconds()<10?'0':'')+now.getSeconds()}*`
         });
 
         return true;
