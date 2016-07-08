c489c8ab87d1d596abda2e6e09e2f26aebee0e96
unacceptableUse <unacceptableuse@gmail.com>
2016-07-08 16:57:23 +0100

    Added `!quote`

---
+++ commands/quote.js
@@ -0,0 +1,51 @@
+/**
+ * Created by Peter on 08/07/2016.
+ */
+var r = require('rethinkdb');
+var dateformat = require('dateformat');
+exports.command = {
+    name: "quote",
+    desc: "Get a random quote by a user",
+    usage: "quote <username> [filter]",
+    func: function(user, userID, channel, args, message, bot){
+
+        if(args.length < 2)return false;
+
+        if(args[2]){
+            var sentence = message.substring(message.indexOf(args[2]));
+            r.db('ocelotbot').table('messages').filter({user: args[1]}).filter(r.row('message').match(sentence)).sample(1).run(bot.rconnection, function(err, result){
+                if(err){
+                    bot.sendMessage({
+                        to: channel,
+                        message: "Error: "+err
+                    });
+                }else{
+                    var date = dateformat(new Date(result[0].time), "dd/mm/yy");
+                    bot.sendMessage({
+                        to: channel,
+                        message: `> [${date}] <${result[0].user}> ${result[0].message}`
+                    });
+                }
+            });
+        }else{
+            r.db('ocelotbot').table('messages').filter({user: args[1]}).sample(1).run(bot.rconnection, function(err, result){
+                if(err){
+                    bot.sendMessage({
+                        to: channel,
+                        message: "Error: "+err
+                    });
+                }else{
+                    var date = dateformat(new Date(result[0].time), "dd/mm/yy");
+                    bot.sendMessage({
+                        to: channel,
+                        message: `> [${date}] <${result[0].user}> ${result[0].message}`
+                    });
+                }
+            });
+        }
+
+
+        return true;
+    }
+};
+
