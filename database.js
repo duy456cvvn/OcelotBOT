43265d38bc6130d0611494381ec83151846735af
unacceptableUse <unacceptableuse@gmail.com>
2016-07-08 12:53:06 +0100

    fixed mysql disconnect error

--- database.js
+++ database.js
@@ -21,33 +21,37 @@ module.exports = function database(bot){
             bot.connection.on('error', function mysqlErrorEvent(err) {
                 bot.log("MySQL Error: " + err);
                 bot.log(err);
-                setTimeout(mysqlInit, 3000);
+                setTimeout(bot.mysqlConnect, 3000);
             });
 
             bot.connection.on('disconnected', function mysqlDisconnectEvent() {
                 bot.log("MySQL Disconnected");
-                setTimeout(mysqlInit, 3000);
+                setTimeout(bot.mysqlConnect, 3000);
             });
 
-            try {
-                bot.connection.connect(function mySqlConnect(err) {
-                    if (err) {
-                        bot.log('Error connecting: ' + err);
-                        if (cb)
-                            cb();
-                        setTimeout(mysqlInit, 3000);
-                    }
-                    else {
-                        bot.log("Connected to MySQL");
-                        if (cb)
-                            cb();
-                    }
 
-                });
-            } catch (e) {
-                bot.log("Exception connecting to MySQL: " + e);
-                setTimeout(mysqlInit, 3000);
-            }
+            bot.mysqlConnect = function mysqlConnect(){
+                try {
+                    bot.connection.connect(function mySqlConnect(err) {
+                        if (err) {
+                            bot.log('Error connecting: ' + err);
+                            if (cb)
+                                cb();
+                            setTimeout(bot.mysqlConnect, 3000);
+                        }
+                        else {
+                            bot.log("Connected to MySQL");
+                            if (cb)
+                                cb();
+                        }
+
+                    });
+                } catch (e) {
+                    bot.log("Exception connecting to MySQL: " + e);
+                    setTimeout(bot.mysqlConnect, 3000);
+                }
+            };
+
 
         }
     }
