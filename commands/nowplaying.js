b65d765f56559190b72778ad16df799ba152cc14
unacceptableUse <unacceptableuse@gmail.com>
2016-07-11 16:14:08 +0100

    Updated nowplaying to use Petify(TM)

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -1,13 +1,32 @@
-var http = require('http');
+var request = require('request');
 exports.command = {
 	name: "nowplaying",
 	desc: "Show whats currently playing",
 	usage: "nowplaying",
 	func: function(user, userID, channel, args, message, bot){
-		 bot.sendMessage({
-    		to: channel,
-    		message: "**Now Playing:** "+bot.nowPlaying
-		});
+
+		request("https://unacceptableuse.com/petermon/music/nowplaying", function(err, resp, body){
+            if(err){
+                bot.sendMessage({
+                	to: channel,
+                	message: err
+                });
+            }else{
+                var data = JSON.parse(body);
+                if(data.title){
+                    bot.sendMessage({
+                        to: channel,
+                        message: "*Now Playing*: "+data.artist+" - "+data.title
+                    })
+                }else{
+                    bot.sendMessage({
+                    	to: channel,
+                    	message: "Nothing is playing! Music is here: https://unacceptableuse.com/petermon/music"
+                    });
+                }
+            }
+        });
+
 		return true;
 		
 	}
