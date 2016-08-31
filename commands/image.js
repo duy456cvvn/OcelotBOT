85bd77b15a6e147324350ebc1ccdedeffaf037d4
unacceptableUse <unacceptableuse@gmail.com>
2016-08-31 20:33:38 +0100

    Added error catcher for !image

--- commands/image.js
+++ commands/image.js
@@ -23,55 +23,62 @@ exports.command = {
 	        });
 
 	        response.on('end', function () {
-	            var data = JSON.parse(body);
-	            if(data.error){
-	            	var message = "";
-	            	switch(data.error){
-	            		case 404:
-	            			message = "Subreddit does not exist or was banned";
-	            			break;
-            			case 403:
-            				message = "Subreddit is invite only or quarantined.";
-            				break;
-            			default:
-            				message = "Unknown error ("+data.error+")";
-            				break;
+				try {
+					var data = JSON.parse(body);
+					if (data.error) {
+						var message = "";
+						switch (data.error) {
+							case 404:
+								message = "Subreddit does not exist or was banned";
+								break;
+							case 403:
+								message = "Subreddit is invite only or quarantined.";
+								break;
+							default:
+								message = "Unknown error (" + data.error + ")";
+								break;
 
-	            	}
+						}
 
-	            	bot.sendMessage({
-			            to: channel,
-			            message: message
-			        });
-	            }else{
-	            	var data = data.data;
-	            	if(data.children.length === 0){
-	            		bot.sendMessage({
-				            to: channel,
-				            message: "There doesn't seem to be anything there."
-			        	});
-	            	}else{
-	            		var posts = data.children;
-	            		var randPost;
+						bot.sendMessage({
+							to: channel,
+							message: message
+						});
+					} else {
+						var data = data.data;
+						if (data.children.length === 0) {
+							bot.sendMessage({
+								to: channel,
+								message: "There doesn't seem to be anything there."
+							});
+						} else {
+							var posts = data.children;
+							var randPost;
 
-	            		for(var i = 0; i < 50; i++){
-	            			var randPost = posts[parseInt(Math.random() * posts.length)];
-            				if(randPost.data.selftext_html === null &&
-								(randPost.data.url.indexOf("imgur.com") > -1 ||
-                                randPost.data.url.indexOf("i.redd.it") > -1 ||
-                                randPost.data.url.indexOf(".png") > -1 ||
-                                randPost.data.url.indexOf(".jp") > -1)){
-            					break;
-            				}
-	            		}
+							for (var i = 0; i < 50; i++) {
+								randPost = posts[parseInt(Math.random() * posts.length)];
+								if (randPost.data.selftext_html === null &&
+									(randPost.data.url.indexOf("imgur.com") > -1 ||
+									randPost.data.url.indexOf("i.redd.it") > -1 ||
+									randPost.data.url.indexOf(".png") > -1 ||
+									randPost.data.url.indexOf(".jp") > -1)) {
+									break;
+								}
+							}
 
-    					bot.sendMessage({
-				            to: channel,
-				            message: i === 50 ? "Couldn't find a post" : randPost.data.title +" - " +randPost.data.url
-			        	});
+							bot.sendMessage({
+								to: channel,
+								message: i === 50 ? "Couldn't find a post" : randPost.data.title + " - " + randPost.data.url
+							});
 
-	            	}
-	            }
+						}
+					}
+				}catch(e){
+					bot.sendMessage({
+						to: channel,
+						message: "Error parsing response from reddit: "+e
+					});
+				}
 	        });
     	});
         return true;
