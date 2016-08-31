324fcb6ffddb7ad0657db7483becabb59bcfd8b3
unacceptableUse <unacceptableuse@gmail.com>
2016-08-31 20:33:19 +0100

    Added generating message to !poem

--- commands/poem.js
+++ commands/poem.js
@@ -9,18 +9,30 @@ exports.command = {
     desc: "Get an urban dictionary definition",
     usage: "defineud <word>",
     func: function(user, userID, channel, args, message, bot){
+
+        var messageID = 0;
+        bot.sendMessage({
+            to: channel,
+            message: "Generating Poem (This will take forever)"
+        }, function(err, resp){
+            if(!err)
+                messageID = resp.ts;
+        });
+
         r.db('ocelotbot').table('messages').filter(function(message){
-            return message("message").match(".*([to]o|u|u[ew]|[ei]w|2)$").and(r.gt(message('message').split(" ").count(), 5))
+            return message("message").match(".*([to]o|u|[uei]w|2)$").and(r.gt(message('message').split(" ").count(), 5))
         }).sample(1).pluck("message", "user", "time").run(bot.rconnection, function(err, res){
            if(err){
-               bot.sendMessage({
-               	to: channel,
-               	message: err
+               bot.editMessage({
+                   channel: channel,
+                   messageID: messageID,
+                   message: "Error: "+err
                });
            } else{
-               bot.sendMessage({
-               	to: channel,
-               	message: "Roses are red\nViolets are blue\n>"+res[0].message+"\n - "+res[0].user+" "+new Date(res[0].time).getFullYear()
+               bot.editMessage({
+                   channel: channel,
+                   messageID: messageID,
+                   message: "Roses are red\nViolets are blue\n>"+res[0].message+"\n - "+res[0].user+" "+new Date(res[0].time).getFullYear()
                });
            }
         });
