7ad495fbb07d7a34edd0c4dca26a570c3630d7cb
unacceptableUse <unacceptableuse@gmail.com>
2016-09-11 14:20:47 +0100

    emojis

--- commands/status.js
+++ commands/status.js
@@ -8,15 +8,15 @@ exports.command = {
     func: function(user, userID, channel, args, message, bot){
 
         var output = "*Service Status:*\n";
-        var statusMonitor = bot.services.loadAfter[4]; //Kekkity kek
+        var statusMonitor = bot.services.loadAfter[3]; //Kekkity kek
         if(statusMonitor){
             for(var i in statusMonitor.errors)
                 if(statusMonitor.errors.hasOwnProperty(i))
-                    output += i+": "+(statusMonitor.errors[i].length == 0 ? "no" : statusMonitor.errors[i].length)+" errors\n"
+                    output += (statusMonitor.errors.length > 0 ? ":red_circle: " : ":white_check_mark: ")+i+"\n"
         }
 
-        output += "RethinkDB: "+(bot.rconnection ? "Connected" : "*NOT CONNECTED*")+"\n";
-        output += "MySQL: "+(bot.connection ? "Connected" : "*NOT CONNECTED*")+"\n";
+        output += (bot.rconnection  ? ":white_check_mark:" : ":red_circle:")+" RethinkDB\n";
+        output += (bot.connection  ? ":white_check_mark:" : ":red_circle:")+" MySQL\n";
 
         bot.sendMessage({
         	to: channel,
