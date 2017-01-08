8653208feeab24f103e9a3167ffa4c964d16d264
Joel Margolis <joel@silocode.net>
2017-01-08 16:59:13 -0500

    fix stats command

--- commands/stats.js
+++ commands/stats.js
@@ -11,91 +11,94 @@ exports.command = {
     func: function(user, userID, channel, args, message, bot) {
         if(args.length == 2) {            
             var target = args[1].toLowerCase();
-
-            var messageID;
+            
             bot.sendMessage({
                 to: channel,
                 message: "Gathering stats (This might take a while)..."
             }, function(err, resp) {
                 if(!err) {
-                    messageID = resp.ts
-                }
-            });
-             
-            var output = [`*Overview for ${target}*:`],
-                totalMessages = 0,
-                totalWords = 0,
-                totalChars = 0,
-                emojis = {},
-                totalEmojis = 0,
-                uniqueWords = {},
-                channels = {};
+                    var messageID = resp.ts,
+                        output = [`*Overview for ${target}*:`],
+                        totalMessages = 0,
+                        totalWords = 0,
+                        totalChars = 0,
+                        emojis = {},
+                        totalEmojis = 0,
+                        uniqueWords = {},
+                        channels = {};
 
-            bot.connection.query('SELECT * FROM Messages WHERE user = ?', [target], function(err, result) {
-                if(err) {
-                    output.push(`Error getting messages: ${err}`);
-                } else {
-                    totalMessages = result.length;
-                    result.forEach(function(row) {
-                        var message = row.message,
-                            chan = row.channel,
-                            words = message.split(' ');
+                    bot.connection.query('SELECT * FROM Messages WHERE user = ?', [target], function(err, result) {
+                        if(err) {
+                            output.push(`Error getting messages: ${err}`);
+                        } else {
+                            totalMessages = result.length;
+                            bot.log(`Total Messages: ${totalMessages}`);
+                            result.forEach(function(row) {
+                                var message = row.message,
+                                    chan = row.channel,
+                                    words = message.split(' ');
 
-                        totalWords += words.length;
-                        totalChars += message.length;
-                        words.forEach(function(w) {
-                            if (w.length > 3 && commonWords.indexOf(w) == -1) {
-                                if(!(w in uniqueWords)) {
-                                    uniqueWords[w] = 0;
+                                totalWords += words.length;
+                                totalChars += message.length;
+                                words.forEach(function(w) {
+                                    if (w.length > 3 && commonWords.indexOf(w) == -1) {
+                                        if(!(w in uniqueWords)) {
+                                            uniqueWords[w] = 0;
+                                        }
+                                        uniqueWords[w]++;
+                                    }
+                                });
+
+                                if(!(chan in channels)) {
+                                    channels[chan] = 0;
                                 }
-                                uniqueWords[w]++;
-                            }
-                        });
+                                channels[chan]++;
 
-                        if(!(chan in channels)) {
-                            channels[chan] = 0;
-                        }
-                        channels[chan]++;
-
-                        var emojiRegex = message.match(/:[^:]+:/g);
-                        if(emojiRegex && emojiRegex.length > 0) {
-                            emojiRegex.forEach(function(e) {
-                                if(!(e in emojis)) {
-                                    emojis[e] = 0;
+                                var emojiRegex = message.match(/:[^:]+:/g);
+                                if(emojiRegex && emojiRegex.length > 0) {
+                                    emojiRegex.forEach(function(e) {
+                                        if(!(e in emojis)) {
+                                            emojis[e] = 0;
+                                        }
+                                        emojis[e]++;
+                                    });
+                                    totalEmojis += emojiRegex.length;
                                 }
-                                emojis[e]++;
                             });
-                            totalEmojis += emojiRegex.length;
+
+                            bot.log("Finished parsing messages")
+
+                            var uniqueWordsSorted = Object.keys(uniqueWords).sort(function(a, b) {
+                                return uniqueWords[a] - uniqueWords[b]
+                            });
+                            var channelsSorted = Object.keys(channels).sort(function(a, b) {
+                                return channels[a] - channels[b]
+                            });
+
+                            var emojisSorted = Object.keys(emojis).sort(function(a, b) {
+                                return emojis[a] - emojis[b]
+                            });
+
+                            output = output.concat([
+                                `- *${totalMessages}* total messages.`,
+                                `- *${totalWords}* total words (*${Object.keys(uniqueWords).length}* unique).`,
+                                `- *${parseInt(totalWords / totalMessages)}* words per message.`,
+                                `- *${totalChars}* total characters.`,
+                                `- At *44* words per minute, this would've taken *${parseInt((totalWords / 44) / 60)}* hours to type.`,
+                                `- All their messages in a text file would take *${parseInt(totalChars / 1024)}* kb`,
+                                `- The most used word is *'${uniqueWordsSorted[uniqueWordsSorted.length - 1]}'* with *${uniqueWords[uniqueWordsSorted[uniqueWordsSorted.length - 1]]}* uses.`,
+                                `- Their favourite emoji is *${emojisSorted[emojisSorted.length-1]}*, having used it *${emojis[emojisSorted[emojisSorted.length-1]]}* times. They have used *${emojisSorted.length}* different emojis, *${totalEmojis}* total times.`,
+                                `- Their favourite channel is *${channelsSorted[channelsSorted.length - 1]}* with *${channels[channelsSorted[channelsSorted.length - 1]]}* messages.`
+                            ]);
+
+                            bot.log(`Okay, done. The message ID is ${messageID}. Here's the message: ${output.join('\n')}`);
+
+                            bot.editMessage({
+                                channel: channel,
+                                messageID: messageID,
+                                message: output.join('\n')
+                            });
                         }
-                    });
-
-                    var uniqueWordsSorted = Object.keys(uniqueWords).sort(function(a, b) {
-                        return uniqueWords[a] - uniqueWords[b]
-                    });
-                    var channelsSorted = Object.keys(channels).sort(function(a, b) {
-                        return channels[a] - channels[b]
-                    });
-
-                    var emojisSorted = Object.keys(emojis).sort(function(a, b) {
-                        return emojis[a] - emojis[b]
-                    });
-
-                    output = output.concat([
-                        `- *${totalMessages}* total messages.`,
-                        `- *${totalWords}* total words (*${Object.keys(uniqueWords).length}* unique).`,
-                        `- *${parseInt(totalWords / totalMessages)}* words per message.`,
-                        `- *${totalChars}* total characters.`,
-                        `- At *44* words per minute, this would've taken *${parseInt((totalWords / 44) / 60)}* hours to type.`,
-                        `- All their messages in a text file would take *${parseInt(totalChars / 1024)}* kb`,
-                        `- The most used word is *'${uniqueWordsSorted[uniqueWordsSorted.length - 1]}'* with *${uniqueWords[uniqueWordsSorted[uniqueWordsSorted.length - 1]]}* uses.`,
-                        `- Their favourite emoji is *${emojisSorted[emojisSorted.length-1]}*, having used it *${emojis[emojisSorted[emojisSorted.length-1]]}* times. They have used *${emojisSorted.length}* different emojis, *${totalEmojis}* total times.`,
-                        `- Their favourite channel is *${channelsSorted[channelsSorted.length - 1]}* with *${channels[channelsSorted[channelsSorted.length - 1]]}* messages.`
-                    ]);
-
-                    bot.editMessage({
-                        channel: channel,
-                        messageID: messageID,
-                        message: output.join('\n')
                     });
                 }
             });
