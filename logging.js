907b76188d98f022076f18cd27ddf4257d5f5c07
Joel Margolis <joel@silocode.net>
2017-01-08 12:33:08 -0500

    convert logging to MySQL

--- logging.js
+++ logging.js
@@ -1,51 +1,60 @@
 /**
  * Created by Peter on 07/07/2016.
  */
-var r = require('rethinkdb');
-var CLIENT_EVENTS   = require('@slack/client').CLIENT_EVENTS;
-module.exports = function logging(bot){
+
+var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+
+module.exports = function(bot) {
     var userList = {};
     return {
-        name: "Message Logging",
-        init: function init(cb){
-            bot.log("Initialising message logging...");
+        name: 'Message Logging',
+        init: function(callback) {
+            bot.log('Initializing message logging...');
 
-            bot.rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function rtmOpenEvent(data) {
-                bot.web.users.list(function getUserList(err, list) {
+            bot.rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function(data) {
+                bot.web.users.list(function(err, list) {
                     if (err || !list.ok) {
-                        bot.error("Error getting user list: " + err)
+                        bot.error(`Error getting user list: ${err}`);
                     } else {
-                        for (var i in list.members) {
-                            if (list.members.hasOwnProperty(i)) {
+                        for(var i in list.members) {
+                            if(list.members.hasOwnProperty(i)) {
                                 var user = list.members[i];
-                                if (!user.deleted) {
+                                if(!user.deleted) {
                                     userList[user.id] = user.name;
                                 }
                             }
                         }
-                        bot.log("Aquired user list.");
+                        bot.log('Aquired user list.');
                     }
                 });
             });
 
 
-            bot.registerMessageHandler("logging", function logMessages(message, channelID, user, userID){
-                if(userID){
-                    var messageObj = {channel: channelID, user: userList[userID] ? userList[userID]  : userID , message: message, time: new Date().getTime()};
-                    r.db('ocelotbot').table('messages').insert(messageObj).run(bot.rconnection, function logMessageQuery(err){
-                        if(err){
-                            bot.error("Error logging message: "+err);
+            bot.registerMessageHandler('logging', function(message, channelID, user, userID) {
+                if(userID) {
+                    var userInList = (userID in userList),
+                        args = [
+                            channelID,
+                            userInList ? userList[userID] : userID,
+                            message,
+                            new Date().getTime()
+                        ];
+
+                    bot.connection.query('INSERT INTO Messages (channel, user, message, time) VALUES (?, ?, ?, ?)', args, function(err, result) {
+                        if(err) {
+                            bot.error(`Error logging message: ${err}`);
                         }
 
-                        if(!userList[userID]){
-                            bot.warn("WARNING: "+userID+" has no attached username. ("+message+")");
+                        if(!userInList) {
+                            bot.warn(`WARNING: ${userID} has no attached username. (${message})`);
                         }
-                    })
+                    });
                 }
             });
 
-            if(cb)
-                cb();
+            if(callback) {
+                callback();
+            }
         },
 
         userList: userList
