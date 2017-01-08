585a93596139270d73f6a552198d3a55b3f98259
Joel Margolis <joel@silocode.net>
2017-01-08 13:23:08 -0500

    convert markov command to MySQL

--- commands/markov.js
+++ commands/markov.js
@@ -1,93 +1,73 @@
 var markov = require('markov');
-var r = require("rethinkdb");
 var async = require('async');
+
 exports.command = {
 	name: "markov",
 	desc: "Generate a markov chain based on a user",
 	usage: "markov <user> [sample] [length] [key]",
 	func: function(user, userID, channel, args, message, bot){
-		if(args.length < 2)return false;
+		if(args.length < 2) {
+            return false;
+        }
 
         var messageID = 0;
         bot.sendMessage({
         	to: channel,
         	message: "Generating Markov Chain (This might take a while)"
         }, function(err, resp){
-            if(!err)
+            if(!err) {
                 messageID = resp.ts;
+            }
         });
 
         var sample = args[3] && !isNaN(args[3]) ? parseInt(args[3]) : 100;
         var length = args[2] && !isNaN(args[3]) ? parseInt(args[2]) : 35;
 
-        r.db("ocelotbot").table("messages").filter({user: args[1]}).pluck("message").sample(sample).run(bot.rconnection, function markovQuery(err, cursor){
-            if(err){
+        bot.connection.query('SELECT message FROM Messages WHERE user = ? LIMIT ?', [args[1], sample], function(err, result) {
+            if(err) {
                 bot.editMessage({
-                    channel: channel,
+                    channel: channel, 
                     messageID: messageID,
-                    message: "Error: "+err
+                    message: `Error: ${err}`
                 });
-            }else{
-                bot.log("Retrieved messages");
-                var m = markov();
-                var count = 0;
-                cursor.toArray(function(err, array){
-                    if(err){
+            } else {
+                bot.log('Retrieved messages');
+
+                var m = markov(),
+                    count = 0;
+                
+                async.eachSeries(result, function(row, cb) {
+                    m.seed(row.message, function() {
+                        count++;
                         bot.editMessage({
                             channel: channel,
                             messageID: messageID,
-                            message: "Error: "+err
-                        });
-                    }else{
-                        async.eachSeries(array, function(row, cb){
-                            m.seed(row.message, function(){
-                                count++;
-                                bot.editMessage({
-                                    channel: channel,
-                                    messageID: messageID,
-                                    message: "Generating Markov Chain (Seeded "+Math.floor((count/sample)*100)+"%)"
-                                }, cb);
-                            });
+                            message: `Generating Markov Chain (Seeded ${Math.floor((count / sample) * 100)}%)`
+                        }, cb);
+                    });
+                }, function() {
+                    var messages = [
+                        m.forward(args[4] ? args[4] : m.pick(), length).join(" "),
+                        m.forward(args[4] ? args[4] : m.pick(), length).join(" "),
+                        m.forward(args[4] ? args[4] : m.pick(), length).join(" "),
+                        m.forward(args[4] ? args[4] : m.pick(), length).join(" "),
+                        m.forward(args[4] ? args[4] : m.pick(), length).join(" "),
+                        m.forward(args[4] ? args[4] : m.pick(), length).join(" "),
+                        m.forward(args[4] ? args[4] : m.pick(), length).join(" "),
+                        m.forward(args[4] ? args[4] : m.pick(), length).join(" "),
+                        m.forward(args[4] ? args[4] : m.pick(), length).join(" "),
+                        m.forward(args[4] ? args[4] : m.pick(), length).join(" "),
+                    ];
 
-                        }, function(){
-
-                            var messages = [
-                                m.forward(args[4] ? args[4] : m.pick(), length).join(" "),
-                                m.forward(args[4] ? args[4] : m.pick(), length).join(" "),
-                                m.forward(args[4] ? args[4] : m.pick(), length).join(" "),
-                                m.forward(args[4] ? args[4] : m.pick(), length).join(" "),
-                                m.forward(args[4] ? args[4] : m.pick(), length).join(" "),
-                                m.forward(args[4] ? args[4] : m.pick(), length).join(" "),
-                                m.forward(args[4] ? args[4] : m.pick(), length).join(" "),
-                                m.forward(args[4] ? args[4] : m.pick(), length).join(" "),
-                                m.forward(args[4] ? args[4] : m.pick(), length).join(" "),
-                                m.forward(args[4] ? args[4] : m.pick(), length).join(" "),
-                            ];
-
-                            bot.editMessage({
-                                channel: channel,
-                                messageID: messageID,
-                                message: "> "+ messages.join("\n>")
-                            });
-
-
-                            // async.retry(10, function(cb){
-                            //     var chain = m.forward(args[4] ? args[4] : m.pick(), length);
-                            //     if(chain.length < 2)cb(chain);
-                            //     else cb(null, chain.join(" "))
-                            // }, function(err, result){
-                            //     bot.editMessage({
-                            //         channel: channel,
-                            //         messageID: messageID,
-                            //         message: "> "+(err ? "Insufficient data for meaningful answer ("+err+")" : result)
-                            //     });
-                            // });
-
-                        });
-                    }
+                    bot.editMessage({
+                        channel: channel,
+                        messageID: messageID,
+                        message: `>${messages.join('\n>')}`
+                    });
                 });
             }
         });
-       return true;
+
+        return true;
 	}
 };
