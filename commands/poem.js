c06c9c46841a4fcf8a416ff20093f23bd7620751
Joel Margolis <joel@silocode.net>
2017-01-08 17:03:35 -0500

    fix poem command

--- commands/poem.js
+++ commands/poem.js
@@ -8,36 +8,36 @@ exports.command = {
             to: channel,
             message: "Generating Poem (This will take forever)"
         }, function(err, resp){
-            if(!err)
+            if(!err) {
                 messageID = resp.ts;
-        });
+                bot.connection.query('SELECT message, user, time FROM Messages WHERE message REGEXP ".*([to]o|u|[uei]w|2)$" AND (LENGTH(message) - LENGTH(REPLACE(message, " ", ""))) > 5 LIMIT 1', function(err, result) {
+                    if(err) {
+                        bot.editMessage({
+                            channel: channel,
+                            messageID: messageID,
+                            message: `Error: ${err}`
+                        });
+                    } else {
+                        if(result.length > 0) {
+                            var row = result[0],
+                                message = [
+                                    'Roses are red',
+                                    'Violets are blue',
+                                    `>${row.message}`,
+                                    ` - ${row.user} ${new Date(row.time).getFullYear()}`
+                                ];
 
-        bot.connection.query('SELECT message, user, time FROM Messages WHERE message REGEXP ".*([to]o|u|[uei]w|2)$" AND (LENGTH(message) - LENGTH(REPLACE(message, " ", ""))) > 5 LIMIT 1', function(err, result) {
-            if(err) {
-                bot.editMessage({
-                    channel: channel,
-                    messageID: messageID,
-                    message: `Error: ${err}`
+                            bot.editMessage({
+                                channel: channel,
+                                messageID: messageID,
+                                message: message.join('\n')
+                            });
+                        }
+                    }
                 });
-            } else {
-                if(result.length > 0) {
-                    var row = result[0],
-                        message = [
-                            'Roses are red',
-                            'Violets are blue',
-                            `>${row.message}`,
-                            ` - ${row.user} ${new Date(row.time).getFullYear()}`
-                        ];
-
-                    bot.editMessage({
-                        channel: channel,
-                        messageID: messageID,
-                        message: message.join('\n')
-                    });
-                }
             }
         });
-        
+
         return true;
     }
 };
