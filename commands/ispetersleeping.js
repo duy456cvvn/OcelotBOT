09e72acdf3a86f3811a838b8643f60b13e3a7167
unacceptableuse <unacceptableuse@gmail.com>
2017-06-11 13:13:35 +0100

    Resurrected the !peterstate command

--- commands/ispetersleeping.js
+++ commands/ispetersleeping.js
@@ -1,61 +1,42 @@
-var http = require('http');
 var request = require('request');
-var session;
 exports.command = {
 	name: "peterstate",
 	desc: "Where's Peter?",
 	usage: "peterstate",
 	func: function(user, userID, channel, args, message, bot){
-        console.log("[ISPETERSLEEPING] Logging in to "+bot.config.petermon.url+"...");
-
-        request.post(
-            {
-                url: bot.config.petermon.url+"login",
-                form: {
-                    username: bot.config.petermon.username,
-                    password: bot.config.petermon.password
-                }
-            },
-            function(err, resp, body){
-                console.log("[ISPETERSLEEPING] Received login response.");
-                if(err){
-                    bot.sendMessage({
-                        to: channel,
-                        message: "There was an error communicating with petermon (during login): `"+err+"`"
-                    });
-                }else{
-                    session = resp.headers['set-cookie'][0].split(";")[0]+";";
-                    request({
-                        url: bot.config.petermon.url+"state/get/info",
-                        headers: {
-                            "Cookie": session
+        bot.connection.query("SELECT * FROM pm_status ORDER BY timestamp DESC LIMIT 1", function(err, result){
+            var peter = result[0];
+            bot.sendAttachment(channel, "", [
+                {
+                    fallback: "...",
+                    color: "#4d41ef",
+                    title: "Peter's last known statistics",
+                    text: `Last heard from petermon at **${peter.timestamp}**`,
+                    fields: [
+                        {
+                            title: "Phone Battery",
+                            value: peter.jimmy_battery+"%",
+                            short: true
+                        },
+                        {
+                            title: "Phone Temperature",
+                            value: peter.jimmy_light+" C",
+                            short: true
+                        },
+                        {
+                            title: "Speed",
+                            value: peter.jimmy_speed+" m/s",
+                            short: true
+                        },
+                        {
+                            title: "Altitude",
+                            value: peter.jimmy_altitude+" m above sealevel.",
+                            short: true
                         }
-                    }, function(err, resp, body){
-                        console.log("[ISPETERSLEEPING] Received state response");
-                        try {
-                            body = JSON.parse(body);
-                            if (err) {
-                                bot.sendMessage({
-                                    to: channel,
-                                    message: "There was an error communicating with petermon (during state get): `" + err + "`"
-                                });
-                            } else {
-                                bot.sendMessage({
-                                    to: channel,
-                                    message: body.description
-                                });
-                            }
-                        }catch(e){
-                            bot.sendMessage({
-                            	to: channel,
-                            	message: "Peter appears to be in an impossible state. Like Schrodinger's cat, he is in all possible states at once.\n"+e
-                            });
-                        }
-                        });
-
+                    ]
                 }
-            });
-		
+            ])
+        });
         return true;
 	},
     test: function(test){
