ac8f3b79d2616b195bc03419e5b179dcc0119d4c
unacceptableuse <unacceptableuse@gmail.com>
2017-06-18 17:51:28 +0100

    Made !status do things again

--- commands/status.js
+++ commands/status.js
@@ -2,28 +2,53 @@
 * Copyright Ocelotworks 2016 - I should be doing homework right now
  */
 const os = require('os');
+var ping = require('ping');
+var async = require('async');
 exports.command = {
     name: "status",
     desc: "Ocelotworks Service Status",
     usage: "status",
     func: function(user, userID, channel, args, message, bot){
 
-        var output = "*Service Status:*\n";
-        var statusMonitor = bot.services.loadAfter[3]; //Kekkity kek
-        if(statusMonitor){
-            for(var i in statusMonitor.errors)
-                if(statusMonitor.errors.hasOwnProperty(i))
-                    output += (statusMonitor.errors.length > 0 ? ":red_circle: " : ":white_check_mark: ")+i+"\n"
-        }
+        const hosts = [
+            {
+                host: "vpn.tekno.pw",
+                name: "VPN (!youtube)"
+            },
+            {
+                host: "earth.boywanders.us",
+                name: "Earth (Main Server)"
+            },
+            {
+                host: "saturn.boywanders.us",
+                name: "Saturn"
+            },
+            {
+                host: "mercury.boywanders.us",
+                name: "Mercury"
+            },
+            {
+                host: "homeof.unacceptableuse.com",
+                name: "Petermon (!peterstate)"
+            },
+        ];
 
-       // output += (bot.rconnection  ? ":white_check_mark:" : ":red_circle:")+" RethinkDB\n";
-        output += (bot.connection  ? ":white_check_mark:" : ":red_circle:")+" MySQL\n";
-        output += (os.loadavg()[0] >= 4 ? ":warning: Earth (High CPU Usage)" : ":white_check_mark: Earth");
+        var output = "**Service Status:**\n";
 
-        bot.sendMessage({
-        	to: channel,
-        	message: output
+        output += (bot.connection  ? ":white_check_mark:" : ":red_circle:")+" **Database**\n";
+
+        async.eachSeries(hosts, function(host, cb){
+            ping.sys.probe(host.host, function(isAlive){
+                output += `${isAlive ? ":white_check_mark:" : ":red_circle:"} **${host.name}**\n`;
+                cb()
+            });
+        }, function(){
+            bot.sendMessage({
+                to: channel,
+                message: output
+            });
         });
+
         return true;
     }
 };
