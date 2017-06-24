0cb4c1b70b6ae3f41e3dd49dec26a90cb607a964
unacceptableuse <unacceptableuse@gmail.com>
2017-06-24 19:34:44 +0100

    Update !crush to allow for using image URLs too

--- commands/crush.js
+++ commands/crush.js
@@ -7,11 +7,12 @@ const request = require('request');
 exports.command = {
     name: "crush",
     desc: "Have a crush on another user",
-    usage: "crush [user]",
+    usage: "crush [user/url]",
     func: function(user, userID, channel, args, message, bot){
         if(!args[1])return false;
         var target = args[1].replace(/[!@<>]/g, "");
-        if(bot.isDiscord && !bot.users[target]){
+        var isUrl = target.startsWith("http");
+        if(bot.isDiscord && !isUrl && !bot.users[target]){
             bot.sendMessage({
                 to: channel,
                 message: "Couldn't find user, did you make sure to @mention them?"
@@ -19,8 +20,8 @@ exports.command = {
         }else{
             if(bot.isDiscord)
                 bot.simulateTyping(channel);
-            var fileName = "temp/avatar-"+target+".png";
-            var outputFile = "temp/crush-"+target+".png";
+            var fileName = "temp/avatar-"+encodeURIComponent(target)+".png";
+            var outputFile = "temp/crush-"+encodeURIComponent(target)+".png";
             if(fs.existsSync(outputFile)){
                 if(bot.isDiscord){
                     bot.uploadFile({
@@ -48,7 +49,9 @@ exports.command = {
             }else if(fs.existsSync(fileName)){
                 makeMeme();
             }else{
-                if(bot.isDiscord){
+                if(isUrl){
+                    request(target).on("end", makeMeme).pipe(fs.createWriteStream(fileName));
+                }else if(bot.isDiscord){
                     var targetUser = bot.users[target];
                     request(`https://cdn.discordapp.com/avatars/${target}/${targetUser.avatar}.png?size=256`).on("end", makeMeme).pipe(fs.createWriteStream(fileName));
                 }else{
@@ -71,7 +74,7 @@ exports.command = {
             function makeMeme(){
                 gm(fileName)
                     .resize(405)
-                    .rotate("black", -5.25)
+                    .rotate("black", -4.7)
                     .extent(600, 875, "-128-455")
                     .toBuffer('PNG', function(err, buffer){
                         if(err){
