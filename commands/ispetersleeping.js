294a91fdb0d9da97249f16908b25f313521e44b9
unacceptableuse <unacceptableuse@gmail.com>
2017-06-29 12:39:23 +0100

    Added now playing data from petify to !peterstate

--- commands/ispetersleeping.js
+++ commands/ispetersleeping.js
@@ -16,47 +16,72 @@ exports.command = {
         };
 
         bot.connection.query("SELECT * FROM pm_status ORDER BY timestamp DESC LIMIT 1", function(err, result){
-            var peter = result[0];
-            bot.sendAttachment(channel, "", [
-                {
-                    fallback: "...",
-                    color: "#4d41ef",
-                    title: `Peter's last state was: ${states[peter.state] || peter.state}`,
-                    text: `Last heard from petermon at ${peter.timestamp}`,
-                    fields: [
-                        {
-                            title: "Heart Rate",
-                            value: peter.peter_heartrate+" BPM",
-                            short: true
-                        },
-                        {
-                            title: "Room Temperature",
-                            value: peter.inside_temp+" C",
-                            short: true
-                        },
-                        {
-                            title: "Phone Battery",
-                            value: peter.jimmy_battery+"%",
-                            short: true
-                        },
-                        {
-                            title: "Phone Temperature",
-                            value: peter.jimmy_light+" C",
-                            short: true
-                        },
-                        {
-                            title: "Speed",
-                            value: peter.jimmy_speed+" m/s",
-                            short: true
-                        },
-                        {
-                            title: "Altitude",
-                            value: peter.jimmy_altitude+" m above sealevel.",
-                            short: true
+            request(`https://unacceptableuse.com/petify/api/${bot.config.petify.apiKey}/nowPlaying/${bot.config.petify.users["139871249567318017"]}`, function(err, resp, body){
+                if(err){
+                    bot.sendMessage({
+                        to: channel,
+                        message: err
+                    });
+                }else{
+                    try {
+                        var data = JSON.parse(body);
+                        if (data.err) {
+                            bot.sendMessage({
+                                to: channel,
+                                message: "Error getting data from Petify:\n```\n"+JSON.stringify(data.err)+"\n```"
+                            });
+                        } else {
+                            var peter = result[0];
+                            bot.sendAttachment(channel, "", [
+                                {
+                                    fallback: "...",
+                                    color: "#4d41ef",
+                                    title: `Peter's last state was: ${states[peter.state] || peter.state}`,
+                                    text: peter.state === "Home" ? `:musical_note: Listening to **${data.artist_name} - ${data.title}**` : `Last update: ${peter.timestamp}`,
+                                    fields: [
+                                        {
+                                            title: "Heart Rate",
+                                            value: peter.peter_heartrate+" BPM",
+                                            short: true
+                                        },
+                                        {
+                                            title: "Room Temperature",
+                                            value: peter.inside_temp+" C",
+                                            short: true
+                                        },
+                                        {
+                                            title: "Phone Battery",
+                                            value: peter.jimmy_battery+"%",
+                                            short: true
+                                        },
+                                        {
+                                            title: "Phone Temperature",
+                                            value: peter.jimmy_light+" C",
+                                            short: true
+                                        },
+                                        {
+                                            title: "Speed",
+                                            value: peter.jimmy_speed+" m/s",
+                                            short: true
+                                        },
+                                        {
+                                            title: "Altitude",
+                                            value: peter.jimmy_altitude+" m above sealevel.",
+                                            short: true
+                                        }
+                                    ]
+                                }
+                            ]);
                         }
-                    ]
+                    }catch(e){
+                        bot.sendMessage({
+                            to: channel,
+                            message: "Error: "+e
+                        });
+                        bot.log(body);
+                    }
                 }
-            ])
+            });
         });
         return true;
 	},
