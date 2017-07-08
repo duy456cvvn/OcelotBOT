1592b6b3ecafcc90340dda619107c1f4c808db28
unacceptableuse <unacceptableuse@gmail.com>
2017-07-08 18:29:46 +0100

    Fixed reloadCommand

--- commands/admin/reloadCommand.js
+++ commands/admin/reloadCommand.js
@@ -5,21 +5,25 @@ module.exports = {
     id: "reloadCommand",
     run: function run(user, userID, channel, message, args, event, bot, recv){
         try {
-            bot.util.uncache("../"+args[2], function () {
-                var loadedCommand = require("../" + args[2]);
-                recv.sendMessage({
-                    to: channel,
-                    message: `Loaded command ${loadedCommand.name}`
-                });
-                bot.commandUsages[loadedCommand.name] = {
-                    usage: loadedCommand.usage,
-                    accessLevel: loadedCommand.accessLevel
-                };
-                for (var i in loadedCommand.commands) {
-                    if (loadedCommand.commands.hasOwnProperty(i)) {
-                        bot.commands[loadedCommand.commands[i]] = loadedCommand.run;
+            const path = __dirname+"/../"+args[2];
+            var loaded = false;
+            bot.util.uncache(path, function () {
+                if(!loaded) {
+                    var loadedCommand = require(path);
+                    recv.sendMessage({
+                        to: channel,
+                        message: `Loaded command ${loadedCommand.name}`
+                    });
+                    bot.commandUsages[loadedCommand.name] = {
+                        usage: loadedCommand.usage,
+                        accessLevel: loadedCommand.accessLevel
+                    };
+                    for (var i in loadedCommand.commands) {
+                        if (loadedCommand.commands.hasOwnProperty(i)) {
+                            bot.commands[loadedCommand.commands[i]] = loadedCommand.run;
+                        }
                     }
-
+                    loaded = true;
                 }
             });
         }catch(e){
