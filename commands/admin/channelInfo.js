c7430878cfb26e940192d89de1c07e8e9f00cda0
unacceptableuse <unacceptableuse@gmail.com>
2017-07-08 18:30:13 +0100

    Fixed !admin channelInfo

--- commands/admin/channelInfo.js
+++ commands/admin/channelInfo.js
@@ -4,26 +4,28 @@
 module.exports = {
     id: "channelinfo",
     run: function run(user, userID, channel, message, args, event, bot, recv){
-        const channelID = recv.getServerFromChannel(args[1]);
-        const thisChannel =  recv.getChannelInfo(channelID);
-        const thisServer = recv.getServerInfo(channelID);
-        if(!channelID){
-            recv.sendMessage({
-                to: channel,
-                message: "This channel does not exist."
+        recv.getServerFromChannel(args[1], function(err, thisServer){
+            recv.getChannelInfo(args[1], function(err, thisChannel){
+                recv.getServerInfo(thisServer, function(err, thisServer){
+                    if(!channelID){
+                        recv.sendMessage({
+                            to: channel,
+                            message: "This channel does not exist."
+                        });
+                    }else if(channelID === "DM"){
+                        var thisUser = recv.getUser(channelID);
+                        recv.sendMessage({
+                            to: channel,
+                            message: `This is a direct message from ${thisUser.username}`
+                        });
+                    }else{
+                        recv.sendMessage({
+                            to: channel,
+                            message: `Channel ${thisChannel.name} belongs to server ${thisServer.name}. It has ${thisServer.member_count} members.`
+                        });
+                    }
+                });
             });
-        }else if(channelID === "DM"){
-            var thisUser = recv.getUser(channelID);
-            recv.sendMessage({
-                to: channel,
-                message: `This is a direct message from ${thisUser.username}`
-            });
-        }else{
-            recv.sendMessage({
-                to: channel,
-                message: `Channel ${thisChannel.name} belongs to server ${thisServer.name}. It has ${thisServer.member_count} members.`
-            });
-        }
-
+        });
     }
 };
