1a90bb52617aa711b7ff21073a37f7b3e4978d10
unacceptableuse <unacceptableuse@gmail.com>
2017-07-08 23:35:42 +0100

    Fixed !admin channelInfo

--- commands/admin/channelInfo.js
+++ commands/admin/channelInfo.js
@@ -4,27 +4,27 @@
 module.exports = {
     id: "channelinfo",
     run: function run(user, userID, channel, message, args, event, bot, recv){
-        recv.getServerFromChannel(args[1], function(err, thisServer){
-            recv.getChannelInfo(args[1], function(err, thisChannel){
-                recv.getServerInfo(thisServer, function(err, thisServer){
-                    if(!channelID){
-                        recv.sendMessage({
-                            to: channel,
-                            message: "This channel does not exist."
-                        });
-                    }else if(channelID === "DM"){
-                        var thisUser = recv.getUser(channelID);
+        recv.getChannelInfo(args[2], function(err, thisChannel){
+            recv.getServerInfo(thisChannel.guild_id, function(err, thisServer){
+                if(!thisServer){
+                    recv.sendMessage({
+                        to: channel,
+                        message: "This channel does not exist."
+                    });
+                }else if(thisServer === "DM"){
+                    recv.getUser(channelID, function(err, thisUser){
                         recv.sendMessage({
                             to: channel,
                             message: `This is a direct message from ${thisUser.username}`
                         });
-                    }else{
-                        recv.sendMessage({
-                            to: channel,
-                            message: `Channel ${thisChannel.name} belongs to server ${thisServer.name}. It has ${thisServer.member_count} members.`
-                        });
-                    }
-                });
+                    });
+
+                }else{
+                    recv.sendMessage({
+                        to: channel,
+                        message: `Channel **#${thisChannel.name}** belongs to server **${thisServer.name}**. It has ${thisServer.member_count} members.`
+                    });
+                }
             });
         });
     }
