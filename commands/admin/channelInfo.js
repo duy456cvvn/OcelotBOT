82384d2e86cb0b30ea34fc2029ffca9ba8dc6770
unacceptableuse <unacceptableuse@gmail.com>
2017-07-25 18:48:11 +0100

    Add failiure modes to channelInfo command

--- commands/admin/channelInfo.js
+++ commands/admin/channelInfo.js
@@ -5,27 +5,44 @@ module.exports = {
     id: "channelinfo",
     run: function run(user, userID, channel, message, args, event, bot, recv){
         recv.getChannelInfo(args[2], function(err, thisChannel){
-            recv.getServerInfo(thisChannel.guild_id, function(err, thisServer){
-                if(!thisServer){
-                    recv.sendMessage({
-                        to: channel,
-                        message: "This channel does not exist."
-                    });
-                }else if(thisServer === "DM"){
-                    recv.getUser(channelID, function(err, thisUser){
+            if(err){
+                recv.sendMessage({
+                    to: channel,
+                    message: err
+                });
+            }else if(!thisChannel){
+                recv.sendMessage({
+                    to: channel,
+                    message: "That channel doesn't exist."
+                });
+            }else if(!thisChannel.guild_id){
+                recv.sendMessage({
+                    to: channel,
+                    message: "That channel is a DM channel probably"
+                });
+            }else{
+                recv.getServerInfo(thisChannel.guild_id, function(err, thisServer){
+                    if(!thisServer){
                         recv.sendMessage({
                             to: channel,
-                            message: `This is a direct message from ${thisUser.username}`
+                            message: "This channel does not exist."
                         });
-                    });
+                    }else if(thisServer === "DM"){
+                        recv.getUser(channelID, function(err, thisUser){
+                            recv.sendMessage({
+                                to: channel,
+                                message: `This is a direct message from ${thisUser.username}`
+                            });
+                        });
 
-                }else{
-                    recv.sendMessage({
-                        to: channel,
-                        message: `Channel **#${thisChannel.name}** belongs to server **${thisServer.name}**. It has ${thisServer.member_count} members.`
-                    });
-                }
-            });
+                    }else{
+                        recv.sendMessage({
+                            to: channel,
+                            message: `Channel **#${thisChannel.name}** belongs to server **${thisServer.name}** (${thisServer.id}). It has ${thisServer.member_count} members.`
+                        });
+                    }
+                });
+            }
         });
     }
 };
