e68af136c8ac92e54ddb02af63a0b564a522c56d
unacceptableuse <unacceptableuse@gmail.com>
2017-08-04 16:27:50 +0100

    Fixed translate crashing if server returns a bad response

--- commands/translate.js
+++ commands/translate.js
@@ -25,40 +25,52 @@ module.exports = {
                     });
                     bot.error("Error getting translation language: " + err);
                 } else {
-                    var langResult = JSON.parse(body);
-                    if (langResult.code === 200) {
-                        request(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${config.get("Commands.translate.key")}&lang=${langResult.lang}-${args[1]}&text=${encodeURIComponent(sentence)}`, function getTranslation(err, response, body) {
-                            if (err) {
-                                recv.sendMessage({
-                                    to: channel,
-                                    message: ":warning: There was an error contacting the translation server."
-                                });
-                                bot.error(`Error translating '${sentence}': ${err}`);
-                            } else {
-                                var transResult = JSON.parse(body);
-                                if (transResult.code === 200) {
-                                    recv.sendMessage({
-                                        to: channel,
-                                        message: "Translated " + transResult.lang + ":\n>" + transResult.text
-                                    });
-                                } else {
+                	try{
+						var langResult = JSON.parse(body);
+						if(langResult.code === 200){
+							request(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${config.get("Commands.translate.key")}&lang=${langResult.lang}-${args[1]}&text=${encodeURIComponent(sentence)}`, function getTranslation(err, response, body){
+								if(err){
+									recv.sendMessage({
+										to: channel,
+										message: ":warning: There was an error contacting the translation server."
+									});
+									bot.error(`Error translating '${sentence}': ${err}`);
+								}else{
+									try{
+										var transResult = JSON.parse(body);
+										if(transResult.code === 200){
+											recv.sendMessage({
+												to: channel,
+												message: "Translated " + transResult.lang + ":\n>" + transResult.text
+											});
+										}else{
 
-                                    recv.sendMessage({
-                                        to: channel,
-                                        message: ":warning: Error: " + langResult.message + "\nYou must supply an output language i.e 'en'"
-                                    });
-                                }
-                            }
-                        });
-
-
-                    } else {
-                        recv.sendMessage({
-                            to: channel,
-                            message: `:warning: Error guessing language (${langResult.code}). Try again later`
-                        });
-                        bot.error(body);
-                    }
+											recv.sendMessage({
+												to: channel,
+												message: ":warning: Error: " + langResult.message + "\nYou must supply an output language i.e 'en'"
+											});
+										}
+									}catch(e){
+										recv.sendMessage({
+											to: channel,
+											message: ":bangbang: Received a bad response from the translate server. Try again later."
+										});
+									}
+								}
+							});
+						}else{
+							recv.sendMessage({
+								to: channel,
+								message: `:warning: Error guessing language (${langResult.code}). Try again later`
+							});
+							bot.error(body);
+						}
+					}catch(e){
+						recv.sendMessage({
+							to: channel,
+							message: ":bangbang: Received a bad response from the translate server. Try again later."
+						});
+					}
                 }
             });
         }
