6a1978a4602f8380124435649af58fb076d3bb7b
unacceptableuse <unacceptableuse@gmail.com>
2017-08-07 16:36:38 +0100

    more facts

--- commands/peterstate.js
+++ commands/peterstate.js
@@ -17,7 +17,21 @@ const facts = [
 	"Peter did try and automatically record sex using sensors, but his girlfriend wouldn't allow it.",
 	"Peter plays <:Overwatch:230070017390149632> **Overwatch**: Peter#25877",
 	"Peter once got stranded for **4 hours** in a town within walking distance of his house.",
-	"Bragging about **Reddit Karma** is not a good way to impress a girl."
+	"Bragging about **Reddit Karma** is not a good way to impress a girl.",
+	"Peter has a weird ability to remember numbers that contain a **9**.",
+	"Peter works for a company his mum compared to the **Hitler Youth**.",
+	"Hiding a **fire extinguisher** in your room is not a good way to impress a girl.",
+	"Peter kept track of how many times his ex got mad at him. The longest break between arguments was **5 days**",
+	"Peter once pretended a girl was his **sister** purely for banter.",
+	"Some times Peter remembers a **snail** he stepped on a few years ago and feels sad.",
+	"Peter spends **too much money** on **useless shit**.",
+	"Saying **'Any Hole's A Goal'** is not a good way to impress a girl.",
+	"Peter's average bed time this month is **4AM**",
+	"Peter went to comicon in a **cardboard box**. He'll show you pictures if you ask nicely.",
+	"Peter knows **2^31** off by heart.",
+	"Peter's nickname in primary school was **worm boy**...",
+	"Peter once **fainted** because he stood up for too long.",
+	"Peter once had a **canoe** on his roof."
 ];
 module.exports = {
     name: "Peter State",
