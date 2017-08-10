8363c14ba5fa8ffd6cf6185730fb4ae7ec3bb8be
unacceptableuse <unacceptableuse@gmail.com>
2017-08-10 16:04:07 +0100

    Feedback now goes to feedback channel

--- commands/feedback.js
+++ commands/feedback.js
@@ -6,25 +6,23 @@ module.exports = {
     usage: "feedback [message]",
     accessLevel: 0,
     commands: ["feedback", "support"],
-    run: function run(user, userID, channel, message, args, event, bot, recv) {
-        recv.getServerFromChannel(channel, function(err, server){
-            if(args.length > 1){
+    run: function run(user, userID, channel, message, args, event, bot, recv, debug, server) {
+        if(args.length > 1){
+            recv.sendMessage({
+                to: channel,
+                message: ":white_check_mark: Your feedback has been sent. Please note we cannot respond to all feedback."
+            });
+            recv.getServer(server, function(err, serverInfo){
                 recv.sendMessage({
-                    to: channel,
-                    message: ":white_check_mark: Your feedback has been sent. Please note we cannot respond to all feedback."
+                    to: "344931831151329302",
+                    message: `Feedback from ${userID} (${user}) in ${server} (${serverInfo ? serverInfo.name : "DM"}):\n${message}`
                 });
-                recv.getServer(server, function(err, serverInfo){
-                    recv.sendMessage({
-                        to: "139871249567318017",
-                        message: `Feedback from ${userID} (${user}) in ${server} (${serverInfo ? serverInfo.name : "DM"}):\n${message}`
-                    });
-                });
-            }else{
-                recv.sendMessage({
-                    to: channel,
-                    message: `:bangbang: You must enter some feedback. i.e **${bot.prefixCache[server]}feedback This bot is amazing!**`
-                })
-            }
-        });
+            });
+        }else{
+            recv.sendMessage({
+                to: channel,
+                message: `:bangbang: You must enter some feedback. i.e **${bot.prefixCache[server]}feedback This bot is amazing!**`
+            })
+        }
     }
 };
