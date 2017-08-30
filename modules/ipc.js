aea7719070da107ddadbd753c0ffa04f01d2a3db
unacceptableuse <unacceptableuse@gmail.com>
2017-08-30 14:26:56 +0100

    Clarify conflict message

--- modules/ipc.js
+++ modules/ipc.js
@@ -129,7 +129,7 @@ module.exports = function(bot){
                                     bot.log(`Detected ${conflicts.length} conflicts in ${server.id}`);
                                     bot.receiver.sendMessage({
                                         to: server.id,
-                                        message: `:warning: Heads up: **${conflicts.length}** bots in this server use the same default prefix (!) as I do.\nYou can change my prefix using \`!settings set prefix whatever\` to avoid problems.`
+                                        message: `:warning: Heads up: **${conflicts.length}** bots in this server use the same default prefix (!) as I do.\nIf you're the server owner or have the role 'Bot Master', you can change my prefix using \`!settings set prefix whatever\` to avoid problems.\nIf you've already changed the prefix of the other bot(s), you can ignore this message.`
                                     });
                                 }
                             })
