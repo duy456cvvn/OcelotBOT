a3314f077f5a67496ccbfe0d333495562d6f4bec
unacceptableuse <unacceptableuse@gmail.com>
2017-11-17 11:21:27 +0000

    Fixed !admin nick

--- commands/admin/nick.js
+++ commands/admin/nick.js
@@ -2,10 +2,10 @@ module.exports = {
     id: "nick",
     run: function run(user, userID, channel, message, args, event, bot, recv, debug, server){
         recv.eval(`
-            bot.receivers.discord.internal.client.setNickname({
-                serverID: ${server},
+            bot.receivers.discord.internal.client.editNickname({
+                serverID: "${server}",
                 userID: "146293573422284800",
-                nick: ${args[2]}
+                nick: "${args[2]}"
             });
         `);
     }
