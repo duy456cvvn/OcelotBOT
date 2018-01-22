5f32b686c2de4ec31d2e6e816bcd0196b788e5a5
Peter Maguire <unacceptableuse@gmail.com>
2018-01-22 14:10:14 +0000

    Switch !lamados to lang strings

--- commands/lamados.js
+++ commands/lamados.js
@@ -5,17 +5,17 @@ module.exports = {
 	accessLevel: 0,
 	commands: ["lamados", "sad"],
 	run: function run(user, userID, channel, message, args, event, bot, recv, debug, server) {
-		fs.readFile("./lamados.txt", function(err, data){
+		fs.readFile("./lamados.txt", async function(err, data){
 			if(err){
 				bot.raven.captureException(err);
 				recv.sendMessage({
 					to: channel,
-					message: "Error opening lamados file."
+					message: await bot.lang.getTranslation(server, "LAMADOS_ERROR")
 				});
 			}else{
 				recv.sendMessage({
 					to: channel,
-					message: `Lamados has said "sad" **${data}** times.`
+					message: await bot.lang.getTranslation(server, "LAMADOS_MESSAGE", data)
 				});
 			}
 		});
